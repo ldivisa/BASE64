531af150593308e92186e87f97eebb16d76e1fdc
Luiz Souza <depositodivisa@gmail.com>
2025-12-25 19:34:42 -0300

    25122025

--- src/main/java/org/hopto/luiztjm/testebase64/TesteBASE64.java
+++ src/main/java/org/hopto/luiztjm/testebase64/TesteBASE64.java
@@ -15,7 +15,7 @@ import java.util.Base64;
 public class TesteBASE64 {
 
     public static void main(String[] args)  {
-        String mensagem = "Hello world 10";
+        String mensagem = "Hello world 25122025";
         byte[] msgCrypto = Base64.getEncoder().encode(mensagem.getBytes());
         System.out.println(Arrays.toString(msgCrypto));
         byte[] msgDecrypto = Base64.getDecoder().decode(msgCrypto);
